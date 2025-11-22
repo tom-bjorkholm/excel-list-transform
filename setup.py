9c13c8e8449247ae53caea79d81ae9a28165f71d
Tom Björkholm <klausuler_linnet0q@icloud.com>
2025-11-22 20:00:13 +0100

    bump version number and dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
   name='excel-list-transform',
-  version='0.8.3',
+  version='0.8.5',
   description='Transform a list in excel or CSV.',
   author='Tom Björkholm',
   author_email='klausuler_linnet0q@icloud.com',
@@ -15,17 +15,17 @@ setup(
   package_data={'excel_list_transform': ['src/py.typed']},
   install_requires=[
     'openpyxl >= 3.1.5',
-    'types-openpyxl >= 3.1.5.20250602',
+    'types-openpyxl >= 3.1.5.20250919',
     'pylightxl >= 1.61',
-    'XlsxWriter >= 3.2.5',
-    'argcomplete >= 3.6.2',
-    'pypi-simple >= 1.6.1',
-    'requests >= 2.32.4',
-    'types-requests >= 2.32.4.20250611',
+    'XlsxWriter >= 3.2.9',
+    'argcomplete >= 3.6.3',
+    'pypi-simple >= 1.8.0',
+    'requests >= 2.32.5',
+    'types-requests >= 2.32.4.20250913',
     'packaging >= 25.0',
-    'pip >= 25.1.1',
+    'pip >= 25.3',
     'setuptools >= 80.9.0',
-    'build >= 1.2.2.post1',
+    'build >= 1.3.0',
     'wheel>=0.45.1'
   ]
 )
